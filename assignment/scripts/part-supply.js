01f69f3ec4544b2ac6926f18e2d4cba5ccfe34b4
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-07-28 21:50:03 -0500

    need to figure out #8, #9 didn't sum negative number properly

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -55,10 +55,19 @@ for ( quantity of supplyChanges ) {
 }
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
-console.log('8. Showing supplyChanges with "while" loop');
 
-/*
+while( supplyChanges[i] > 0) {
+console.log('Added', supplyChanges[i], 'parts.');
+i++;
+}
+
+//console.log('8. Showing supplyChanges with "while" loop');
+
+
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('9. Total supplies available is:');
-*/
+let totalSupplies = 0;
+for (i=0; i<supplyChanges.length; i++) {
+  totalSupplies += supplyChanges[i];
+  console.log('9. Total supplies available is :', totalSupplies);
+} // REVISIT totalSupplies didn't account for -6 
