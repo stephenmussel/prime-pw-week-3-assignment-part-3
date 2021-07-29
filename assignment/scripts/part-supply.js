8dfa20bd8ee9673e65cfa0bf52b39354533a23dd
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-07-28 20:39:25 -0500

    updated css, changed strings to numbers, and working thru #6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -8,11 +8,11 @@ console.log('1. Number of partsNeeded:', partsNeeded);
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-let supplyChanges = [ '3', '5', '-6', '0', '7', '11' ];
+let supplyChanges = [ 3, 5, -6, 0, 7, 11 ];
 console.log('2. Array of supplyChanges:', supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:', supplyChanges[1]);
+console.log('3. Second supplyChanges is:', supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
@@ -23,7 +23,7 @@ console.log('4. Removed item:', removedItem);
 // console.log('4. Removed item:', supplyChanges.pop());
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-addingItem = supplyChanges.push('25');
+addingItem = supplyChanges.push(25);
 console.log('5. Adding 25 to supplyChanges.', supplyChanges); // REVIST this one again
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
@@ -31,8 +31,16 @@ console.log('5. Adding 25 to supplyChanges.', supplyChanges); // REVIST this one
 //    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
-console.log('6. Showing supplyChanges...');
+// console.log('6. Showing supplyChanges...');
 
+for (var i=0; i<6; i++)
+  if (i > 0) {
+  console.log('Added', supplyChanges[i], 'parts');
+} else if (i = 0) {
+  console.log('No change.');
+} else if (i < 0) {
+  console.log('Removed', supplyChanges[i], 'parts');
+}
 /*
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
